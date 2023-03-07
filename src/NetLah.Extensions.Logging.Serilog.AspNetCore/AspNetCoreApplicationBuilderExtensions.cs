fd5d9e61da9b820efc470abfd10c30a3c7de4bb6
Tho Ho <code@tho.email>
2023-03-07 15:21:41 +0800

    UseSerilogRequestLogging, only log error when exception or code>=500; duration>=500 become warning

--- src/NetLah.Extensions.Logging.Serilog.AspNetCore/AspNetCoreApplicationBuilderExtensions.cs
+++ src/NetLah.Extensions.Logging.Serilog.AspNetCore/AspNetCoreApplicationBuilderExtensions.cs
@@ -3,27 +3,35 @@ using Serilog;
 using Serilog.Events;
 using FrameworkLogLevel = Microsoft.Extensions.Logging.LogLevel;
 
-namespace NetLah.Extensions.Logging
+namespace NetLah.Extensions.Logging;
+
+public static class AspNetCoreApplicationBuilderExtensions
 {
-    public static class AspNetCoreApplicationBuilderExtensions
+    public static IApplicationBuilder UseSerilogRequestLoggingLevel(this IApplicationBuilder applicationBuilder, FrameworkLogLevel logLevel = FrameworkLogLevel.Debug)
     {
-        public static IApplicationBuilder UseSerilogRequestLoggingLevel(this IApplicationBuilder applicationBuilder, FrameworkLogLevel logLevel = FrameworkLogLevel.Debug)
+        if (logLevel == FrameworkLogLevel.None)
         {
-            if (logLevel == FrameworkLogLevel.None)
-                return applicationBuilder;
-
-            var logEventLevel = logLevel switch
-            {
-                FrameworkLogLevel.Trace => LogEventLevel.Verbose,
-                FrameworkLogLevel.Debug => LogEventLevel.Debug,
-                FrameworkLogLevel.Information => LogEventLevel.Information,
-                FrameworkLogLevel.Warning => LogEventLevel.Warning,
-                FrameworkLogLevel.Error => LogEventLevel.Error,
-                FrameworkLogLevel.Critical => LogEventLevel.Fatal,
-                _ => throw new NotSupportedException("Loglevel " + logLevel),
-            };
-
-            return applicationBuilder.UseSerilogRequestLogging(opt => opt.GetLevel = (c, d, e) => d >= 500.0 || e != null ? LogEventLevel.Error : logEventLevel);
+            return applicationBuilder;
         }
+
+        var logEventLevel = logLevel switch
+        {
+            FrameworkLogLevel.Trace => LogEventLevel.Verbose,
+            FrameworkLogLevel.Debug => LogEventLevel.Debug,
+            FrameworkLogLevel.Information => LogEventLevel.Information,
+            FrameworkLogLevel.Warning => LogEventLevel.Warning,
+            FrameworkLogLevel.Error => LogEventLevel.Error,
+            FrameworkLogLevel.Critical => LogEventLevel.Fatal,
+            _ => throw new NotSupportedException("Loglevel " + logLevel),
+        };
+
+#pragma warning disable S3358 // Ternary operators should not be nested
+        return applicationBuilder.UseSerilogRequestLogging(
+            opt => opt.GetLevel =
+                (c, d, e) =>
+                    e != null || c.Response.StatusCode >= 500
+                    ? LogEventLevel.Error
+                    : (d >= 500.0 ? LogEventLevel.Warning : logEventLevel));
+#pragma warning restore S3358 // Ternary operators should not be nested
     }
 }

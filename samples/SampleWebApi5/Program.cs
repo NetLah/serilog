01d03250fc33a0b3181db558ea148713f6be8b4a
Tho Ho <code@tho.email>
2024-01-13 00:33:08 +0800

    Re-support .NET 5.0

---
+++ samples/SampleWebApi5/Program.cs
@@ -0,0 +1,42 @@
+﻿using NetLah.Diagnostics;
+using NetLah.Extensions.Logging;
+
+namespace SampleWebApi;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        ApplicationInfo.Initialize(null);
+        AppLog.InitLogger();
+        try
+        {
+            AppLog.Logger.LogInformation("Application configure...");   // write log console only
+
+            CreateHostBuilder(args).Build().Run();
+        }
+        catch (Exception ex)
+        {
+            AppLog.Logger.LogCritical(ex, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            Serilog.Log.CloseAndFlush();
+        }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .UseSerilog2(logger =>
+            {
+                if (ApplicationInfo.Instance is { } appInfo)
+                {
+                    logger.LogInformation("Application initializing... AppTitle:{appTitle}; Version:{appVersion} BuildTime:{appBuildTime}; Framework:{frameworkName}",
+                        appInfo.Title, appInfo.InformationalVersion, appInfo.BuildTimestampLocal, appInfo.FrameworkName);
+                }
+            })    //  write log to sinks
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}

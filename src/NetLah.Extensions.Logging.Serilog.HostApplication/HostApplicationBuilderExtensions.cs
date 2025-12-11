3abeaa790d7a2510bc3d520cecd33b83e17460aa
Tho Ho <code@tho.email>
2025-12-11 15:12:25 +0800

    Support HostApplicationBuilder since .NET 7.0 (#4)
    * Add release/* branch
    
    * Add NetLah.Extensions.Logging.Serilog.HostApplication

---
+++ src/NetLah.Extensions.Logging.Serilog.HostApplication/HostApplicationBuilderExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using IFrameworkLogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace NetLah.Extensions.Logging;
+
+#if NET8_0_OR_GREATER
+public static class HostApplicationBuilderExtensions
+{
+    public static THostApplicationBuilder UseSerilog<THostApplicationBuilder>(this THostApplicationBuilder hostApplicationBuilder, Action<IFrameworkLogger>? applicationLoggerCreated = null)
+        where THostApplicationBuilder : IHostApplicationBuilder
+    {
+        return hostApplicationBuilder == null
+            ? throw new ArgumentNullException(nameof(hostApplicationBuilder))
+            : hostApplicationBuilder.UseSerilog(builder => AppLog.CreateAppLogger(builder.Configuration),
+            applicationLoggerCreated);
+    }
+
+    public static THostApplicationBuilder UseSerilog<THostApplicationBuilder>(this THostApplicationBuilder hostApplicationBuilder,
+        Action<THostApplicationBuilder, LoggerConfiguration> loggerConfigurationFactory,
+        Action<IFrameworkLogger>? applicationLoggerCreated = null)
+        where THostApplicationBuilder : IHostApplicationBuilder
+    {
+        return hostApplicationBuilder == null
+            ? throw new ArgumentNullException(nameof(hostApplicationBuilder))
+            : loggerConfigurationFactory == null
+            ? throw new ArgumentNullException(nameof(loggerConfigurationFactory))
+            : hostApplicationBuilder
+            .UseSerilog(builder => AppLog.CreateAppLogger(lc => loggerConfigurationFactory(builder, lc)),
+                applicationLoggerCreated);
+    }
+
+    private static THostApplicationBuilder UseSerilog<THostApplicationBuilder>(this THostApplicationBuilder hostApplicationBuilder,
+        Func<THostApplicationBuilder, IFrameworkLogger> configureLogger,
+        Action<IFrameworkLogger>? applicationLoggerCreated = null)
+        where THostApplicationBuilder : IHostApplicationBuilder
+    {
+        var logger = configureLogger(hostApplicationBuilder);
+        applicationLoggerCreated?.Invoke(logger);
+        hostApplicationBuilder.Logging
+            .ClearProviders()
+            .AddSerilog();
+        return hostApplicationBuilder;
+    }
+}
+#else
+public static class HostApplicationBuilderExtensions
+{
+    public static HostApplicationBuilder UseSerilog(this HostApplicationBuilder hostApplicationBuilder, Action<IFrameworkLogger>? applicationLoggerCreated = null)
+    {
+        return hostApplicationBuilder == null
+            ? throw new ArgumentNullException(nameof(hostApplicationBuilder))
+            : hostApplicationBuilder.UseSerilog(builder => AppLog.CreateAppLogger(builder.Configuration),
+            applicationLoggerCreated);
+    }
+
+    public static HostApplicationBuilder UseSerilog(this HostApplicationBuilder hostApplicationBuilder,
+        Action<HostApplicationBuilder, LoggerConfiguration> loggerConfigurationFactory,
+        Action<IFrameworkLogger>? applicationLoggerCreated = null)
+    {
+        return hostApplicationBuilder == null
+            ? throw new ArgumentNullException(nameof(hostApplicationBuilder))
+            : loggerConfigurationFactory == null
+            ? throw new ArgumentNullException(nameof(loggerConfigurationFactory))
+            : hostApplicationBuilder
+            .UseSerilog(builder => AppLog.CreateAppLogger(lc => loggerConfigurationFactory(builder, lc)),
+                applicationLoggerCreated);
+    }
+
+    private static HostApplicationBuilder UseSerilog(this HostApplicationBuilder hostApplicationBuilder,
+        Func<HostApplicationBuilder, IFrameworkLogger> configureLogger,
+        Action<IFrameworkLogger>? applicationLoggerCreated = null)
+    {
+        var logger = configureLogger(hostApplicationBuilder);
+        applicationLoggerCreated?.Invoke(logger);
+        hostApplicationBuilder.Logging
+            .ClearProviders()
+            .AddSerilog();
+        return hostApplicationBuilder;
+    }
+}
+#endif
